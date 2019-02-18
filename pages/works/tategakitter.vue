5a2df9c0f6af3da1d76aa4215e5fc661e4d768ac
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-02-18 09:55:57 +0900

    worksページを設置（作業中）

---
+++ pages/works/tategakitter.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="works-cover-wrpaper">
+      <figure class="image">
+        <img src="/img/works/btnmaker.png" alt="みんなのボタンメーカー">
+      </figure>
+    </div>
+    <div class="container">
+      <h1 class="work-heading">
+        みんなのボタンメーカー
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.works-cover-wrpaper {
+  width: 100%;
+  padding: 4.5em 2em;
+  text-align: center;
+  background: #edf2f7;
+  background-color: #e5e5e5;
+}
+.works-cover-wrpaper .image {
+  margin: 0 auto;
+  /*max-width: calc(800px - 3em);*/
+  max-width: 800px;
+}
+.works-cover-wrpaper .image img {
+  border-radius: 9px;
+}
+@media screen and (max-width: 760px) {
+  .works-cover-wrpaper {
+    padding: 3em 2em;
+  }
+}
+.work-heading {
+  font-size: 1.6em;
+  margin-top: 1.4em;
+  margin-bottom: 1.4em;
+}
+</style>
