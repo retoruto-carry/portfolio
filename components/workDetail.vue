dbd461619f5f5870a8a9be76aceef7fad4d81c79
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-02-20 05:48:11 +0900

    work detailページをコンポーネント化

---
+++ components/workDetail.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="works-cover-wrpaper">
+      <figure class="image">
+        <slot name="cover-image" />
+      </figure>
+    </div>
+    <div class="container">
+      <p class="breadcrumb-work">
+        れとるときゃりーの紹介 > Works
+      </p>
+      <slot name="body" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.works-cover-wrpaper {
+  width: 100%;
+  padding: 4.5em 2em;
+  text-align: center;
+  background: #edf2f7;
+  background-color: #e5e5e5;
+}
+.works-cover-wrpaper .image {
+  margin: 0 auto;
+  /*max-width: calc(800px - 3em);*/
+  max-width: 800px;
+}
+.works-cover-wrpaper .image img {
+  border-radius: 9px;
+}
+@media screen and (max-width: 760px) {
+  .works-cover-wrpaper {
+    padding: 3em 2em;
+  }
+}
+.breadcrumb-work {
+  margin-top: 3em;
+  margin-bottom: 0.2em;
+  color: #777;
+}
+.work-heading {
+  font-size: 1.8em;
+  margin-top: 0.2em;
+  margin-bottom: 1.4em;
+}
+</style>
--- pages/works/tategakitter.vue
+++ pages/works/tategakitter.vue
@@ -1,57 +1,25 @@
 <template>
   <div>
-    <div class="works-cover-wrpaper">
-      <figure class="image">
-        <img src="/img/works/btnmaker.png" alt="みんなのボタンメーカー">
-      </figure>
-    </div>
-    <div class="container">
-      <p class="breadcrumb-work">
-        れとるときゃりーの紹介 > Works
-      </p>
-      <h1 class="work-heading">
-        みんなのボタンメーカー
-      </h1>
-      <h2>
-        概要
-      </h2>
-    </div>
+    <work-detail>
+      <img slot="cover-image" src="/img/works/tategakitter.png" alt="縦書きったー">
+      <template slot="body">
+        <h1 class="work-heading">
+          縦書きったー
+        </h1>
+        <h2>概要</h2>
+      </template>
+    </work-detail>
   </div>
 </template>
 
 <script>
-export default {}
+import workDetail from '@/components/workDetail'
+export default {
+  components: {
+    workDetail
+  }
+}
 </script>
 
 <style>
-.works-cover-wrpaper {
-  width: 100%;
-  padding: 4.5em 2em;
-  text-align: center;
-  background: #edf2f7;
-  background-color: #e5e5e5;
-}
-.works-cover-wrpaper .image {
-  margin: 0 auto;
-  /*max-width: calc(800px - 3em);*/
-  max-width: 800px;
-}
-.works-cover-wrpaper .image img {
-  border-radius: 9px;
-}
-@media screen and (max-width: 760px) {
-  .works-cover-wrpaper {
-    padding: 3em 2em;
-  }
-}
-.breadcrumb-work {
-  margin-top: 3em;
-  margin-bottom: 0.2em;
-  color: #777;
-}
-.work-heading {
-  font-size: 1.8em;
-  margin-top: 0.2em;
-  margin-bottom: 1.4em;
-}
 </style>
